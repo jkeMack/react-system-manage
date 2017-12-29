69891bc492f0921a56e733fec015e49d7b394079
liaoke <215437475@qq.com>
2017-12-29 17:42:35 +0800

    增加表格、增加基本表单页面

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {Login} from "./components/pages/login";
+import {BasicTable} from "./components/pages/basic-table";
+import {BasicForm} from "./components/pages/basic-form";
 import {Router, Route} from 'react-router';
 
 ReactDOM.render(<Router>
     <Route path="/" component={App}>
         <Route path="login" component={Login}/>
-        {/*<Route path="inbox" component={Inbox}>*/}
-        {/*<Route path="messages/:id" component={Message} />*/}
-        {/*</Route>*/}
+        <Route path="table" component={BasicTable}/>
+        <Route path="form" component={BasicForm}/>
     </Route>
 </Router>, document.getElementById('root'));
 registerServiceWorker();
