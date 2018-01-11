f8cad5ddc48bae562806dc68d3153fc70aa6f05d
liaoke <215437475@qq.com>
2018-01-11 11:37:57 +0800

    登录页完善

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,11 +1,32 @@
+import '../style/login.css';
 import React, { Component } from 'react';
+import {Button, Form, Input,} from 'antd';
+import {Link} from 'react-router';
+const FormItem = Form.Item;
 
 export class Login extends Component {
 
     render() {
         return (
             <div className="login">
-               <h2>登录页</h2>
+                <h2 className="title">
+                    后台管理系统
+                </h2>
+                <div className="form">
+                    <Form>
+                        <FormItem>
+                            <Input placeholder="用户名" />
+                        </FormItem>
+                        <FormItem>
+                            <Input type="password" placeholder="密码" />
+                        </FormItem>
+                        <FormItem>
+                            <Button type="primary" htmlType="submit" style={{width:100+'%'}}>
+                                登录
+                            </Button>
+                        </FormItem>
+                    </Form>
+                </div>
             </div>
         );
     }
