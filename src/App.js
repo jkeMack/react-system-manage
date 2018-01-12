42b2360379a9d42250922a2ddc0053044d31a5ae
liaoke <215437475@qq.com>
2018-01-12 18:34:00 +0800

    主体布局样式修改

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, {Component} from 'react';
 import './App.css';
+import React, {Component} from 'react';
 import {Header} from './components/common/header';
 import {Sidebar} from './components/common/sidebar';
 
 class App extends Component {
     render() {
         return (
-            <div>
+            <div className="app">
                 <Header></Header>
                 <Sidebar></Sidebar>
-                <div style={{marginLeft:200+'px',padding:20+'px'}} >{this.props.children}</div>
+                <div className="container">{this.props.children}</div>
             </div>
         );
     }
